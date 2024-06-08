d1b3c56140dde3b7a35482126551c6abd6a14f99
RAGAVE.I <95114993+Ragave103@users.noreply.github.com>
2024-06-08 12:46:15 +0530

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,12 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC3Hdf0uR12-KFV77jAgB1B-ZAcb0Wl5dw",
+  //enter your firebaseconfig here
+  apiKey: "Enter your api key here",
   authDomain: "blockapp-a2639.firebaseapp.com",
   projectId: "blockapp-a2639",
   storageBucket: "blockapp-a2639.appspot.com",
   messagingSenderId: "323467147236",
-  appId: "1:323467147236:web:6923da26520dc81f2f39f1",
+  appId: "Enter the app Id here",
   measurementId: "G-H95EFTYMME",
 };
 
